d9430cdb4d89b12f398a104a87d90de8fd967f2e
nanadotam <nanaamoako202@gmail.com>
2024-04-27 21:51:18 +0000

    added code and reference

--- img-ascii.py
+++ img-ascii.py
@@ -1,1 +1,17 @@
-print("hello, test to GitHub")
+from PIL import Image
+
+def asciiConvert(image, type, saveas, scale):
+    scale = int(scale)
+
+    # open the image and get size
+    img = Image.open(image)
+    w, h = img.size
+
+    # resize the image (downscale)
+    img.resize((w//scale, h//scale)).save("resized.%s" % type)
+
+    #open new image
+    img = image.open("resized.%s" % type)
+    w, h = img.size # get new width and height 
+
+    
